f5c8421c3a210e5c734d6c436069748e8db46b77
Mate Balogh <b.mate11@gmail.com>
2017-10-27 16:27:47 +0200

    Initial commit

---
+++ ConsoleApp2/SortSrting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class SortString
+    {
+        static void Main(string[] args)
+        {
+            string str = "I have a small doggo who cannot behave!";
+            String[] words = str.Split();
+            words.ToList().ForEach(Console.WriteLine);
+            Array.Sort(words);
+            str = string.Join(" ", words);
+            Console.WriteLine(Environment.NewLine + str);
+            Console.ReadKey();
+        }
+    }
+}
